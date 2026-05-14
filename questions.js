1aaec3ae51d33c58b8b267b2d71cacdc37697abb
nilsstahl-svec <nils.stahl@sv-web.de>
2026-05-14 23:38:36 +0200

    Add files via upload

--- questions.js
+++ questions.js
@@ -1,28 +1,6 @@
 export const QUESTIONS = [
   {
     "id": "q1",
-    "title": "Wo ist Stefan aufgewachsen?",
-    "answers": [
-      {
-        "text": "<strong>Bad Urach</strong>",
-        "correct": false
-      },
-      {
-        "text": "<strong>Geislingen an der Steige / Eybach</strong>",
-        "correct": true
-      },
-      {
-        "text": "<strong>Schwäbisch Gmünd</strong>",
-        "correct": false
-      },
-      {
-        "text": "<strong>Freudenstadt</strong>",
-        "correct": false
-      }
-    ]
-  },
-  {
-    "id": "q2",
     "title": "Stefans Lieblingsverein: Welches Stadion ist das?",
     "answers": [
       {
@@ -44,7 +22,7 @@ export const QUESTIONS = [
     ]
   },
   {
-    "id": "q3",
+    "id": "q2",
     "title": "In welcher Stadt hat Debora in Kanada gewohnt?",
     "answers": [
       {
@@ -66,29 +44,29 @@ export const QUESTIONS = [
     ]
   },
   {
-    "id": "q4",
-    "title": "Wo hat Stefan studiert?",
+    "id": "q3",
+    "title": "In welchem Land liegt dieser Ort aus ihrem Missionsalltag?",
     "answers": [
       {
-        "text": "<strong>Tabor Marburg</strong>",
+        "text": "<strong>Uganda</strong>",
         "correct": false
       },
       {
-        "text": "<strong>Theologisches Seminar St. Chrischona</strong>",
-        "correct": false
-      },
-      {
-        "text": "<strong>Internationales Studienzentrum Bad Liebenzell</strong>",
+        "text": "<strong>Malawi</strong>",
         "correct": true
       },
       {
-        "text": "<strong>Albrecht-Bengel-Haus</strong>",
+        "text": "<strong>Kenia</strong>",
+        "correct": false
+      },
+      {
+        "text": "<strong>Tansania</strong>",
         "correct": false
       }
     ]
   },
   {
-    "id": "q5",
+    "id": "q4",
     "title": "In welchem Land waren Stefan und Debora auf Hochzeitsreise?",
     "answers": [
       {
@@ -110,23 +88,23 @@ export const QUESTIONS = [
     ]
   },
   {
-    "id": "q6",
-    "title": "In welchem Land liegt dieser Ort aus ihrem Missionsalltag?",
+    "id": "q5",
+    "title": "Wo ist Stefan aufgewachsen?",
     "answers": [
       {
-        "text": "<strong>Uganda</strong>",
+        "text": "<strong>Bad Urach</strong>",
         "correct": false
       },
       {
-        "text": "<strong>Malawi</strong>",
+        "text": "<strong>Geislingen an der Steige / Eybach</strong>",
         "correct": true
       },
       {
-        "text": "<strong>Kenia</strong>",
+        "text": "<strong>Schwäbisch Gmünd</strong>",
         "correct": false
       },
       {
-        "text": "<strong>Tansania</strong>",
+        "text": "<strong>Freudenstadt</strong>",
         "correct": false
       }
     ]
